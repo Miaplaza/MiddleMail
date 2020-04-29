a17d72aae7e65d662e320058a98c571aa1a1b057
Leonard Kleinhans <lkleinhans@miaplaza.com>
2020-04-30 00:16:36 +0200

    Exception documentation and renaming

---
+++ src/MailService/Exceptions/SingleProcessingException.cs
@@ -0,0 +1,19 @@
+using System;
+using MimeKit;
+
+namespace MiaPlaza.MailService.Exceptions {
+
+	/// <summary>
+	/// The Exception that is thrown when an <see cref="EmailMessage" /> could not be processed
+	/// due to a problem with that specific <see cref="EmailMessage" /> instance.
+	/// The delivery will not be retried.static The Exception bubbles up to <see cref="IMessageSource" />.
+	/// Throw a <see cref="GeneralDeliveryException" /> to indicate a general problem.
+	/// </summary>
+	public class SingleDeliveryException : Exception {
+
+		public SingleDeliveryException() { }
+
+		public SingleDeliveryException(string message, EmailMessage emailMessage, Exception innerException)
+			: base($"Exception when sending EmailMessage with Id {emailMessage.Id}: {message}", innerException) { }
+	}
+}
