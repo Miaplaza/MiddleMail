c2cfec0a561d1b9577b317930792bf9ed2e4a1f6
Leonard Kleinhans <lkleinhans@miaplaza.com>
2020-06-15 03:00:44 +0200

    Fix [Required] attribute in ExponentialBackoffOptions

--- src/MiddleMail/ExponentialBackoffOptions.cs
+++ src/MiddleMail/ExponentialBackoffOptions.cs
@@ -5,8 +5,9 @@ using Microsoft.Extensions.Configuration;
 namespace MiddleMail {
 	public class ExponentialBackoffOptions {
 			
-		[Required]
 		public const string SECTION = "MiddleMail:ExponentialBackoff";
+
+		[Required]
 		public int Multiplicator { get; set; }
 	}
 }
