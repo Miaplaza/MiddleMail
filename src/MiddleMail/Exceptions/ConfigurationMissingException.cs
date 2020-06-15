08b73e8959956b8b8cb124521c2e36afd3217962
Leonard Kleinhans <lkleinhans@miaplaza.com>
2020-06-15 03:02:29 +0200

    Remove unused configuration missing exception

--- src/MiddleMail/Exceptions/ConfigurationMissingException.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace MiddleMail.Exceptions {
-	public class ConfigurationMissingException : Exception {
-
-		public ConfigurationMissingException(string configurationKey) : base($"Configuration missing for configuration key {configurationKey}.") { }
-	}
-}
