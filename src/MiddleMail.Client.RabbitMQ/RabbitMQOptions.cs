1d2a18ca2f3246509495cb7395ebd294ba889f87
Leonard Kleinhans <lkleinhans@miaplaza.com>
2020-06-19 00:28:42 +0200

    Required RabbitMQOptions.ConnectionString

--- src/MiddleMail.Client.RabbitMQ/RabbitMQOptions.cs
+++ src/MiddleMail.Client.RabbitMQ/RabbitMQOptions.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MiddleMail.Client.RabbitMQ {
 	public class RabbitMQOptions {
 		public const string SECTION = "MiddleMail:Client:RabbitMQ";
+
+		[Required]
 		public string ConnectionString { get; set; }
 	}
 }
